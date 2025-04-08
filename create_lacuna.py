36fa72a8dc0b89fffa543d75f10353deb2fcdb2e
alexisrenaudin <162428903+alexisrenaudin@users.noreply.github.com>
2025-04-08 16:51:36 +0530

    Update create_lacuna.py
    update to get back the original version of create_lacuna

--- create_lacuna.py
+++ create_lacuna.py
@@ -4,10 +4,8 @@ import numpy as np
 import os
 from tqdm import tqdm
 
-#meta_file = "data/VGG-Face2/meta/identity_meta.csv"
-#train_data_root = "data/VGG-Face2/data/train/"
-meta_file = "/kaggle/input/vggface2/meta/identity_meta.csv"
-train_data_root = "/kaggle/input/vggface2/train/"
+meta_file = "data/VGG-Face2/meta/identity_meta.csv"
+train_data_root = "data/VGG-Face2/data/train/"
 test_data_root = ""
 
 
